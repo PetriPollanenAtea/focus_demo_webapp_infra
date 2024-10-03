fe39aeccf385146119753d97868ad4743ffff660
Petri Pöllänen <petri.pollanen@atea.fi>
2024-10-03 12:39:12 +0300

    Change manual integration setting

--- web_app.tf
+++ web_app.tf
@@ -26,6 +26,6 @@ resource "azurerm_app_service_source_control" "sourcecontrol" {
   app_id             = azurerm_linux_web_app.webapp.id
   repo_url           = "https://github.com/PetriPollanenAtea/focus_demo_webapp_application/"
   branch             = "main"
-  use_manual_integration = true
+  use_manual_integration = false
   use_mercurial      = false
 }
