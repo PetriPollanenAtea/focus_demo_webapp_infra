9f3b18ff305c8669a2c8349c33aaa0c1c81ae2d6
Petri Pöllänen <petri.pollanen@atea.fi>
2024-10-03 09:43:54 +0300

    Fixed GitHub workflow

--- web_app.tf
+++ web_app.tf
@@ -1,28 +1,20 @@
 # Create the Linux App Service Plan
 resource "azurerm_service_plan" "appserviceplan" {
-  name                = "asp-focus-webapp-demo"
+  name                = "asp-focus-demo-webapp"
   location            = azurerm_resource_group.rg_web_app.location
   resource_group_name = azurerm_resource_group.rg_web_app.name
-  os_type             = "Windows"
+  os_type             = "Linux"
   sku_name            = "B1"
 }
 
 # Create the web app, pass in the App Service Plan ID
-resource "azurerm_windows_web_app" "webapp" {
-  name                  = "focus-webapp-demo"
+resource "azurerm_linux_web_app" "webapp" {
+  name                  = "focus-demo-webapp"
   location              = azurerm_resource_group.rg_web_app.location
   resource_group_name   = azurerm_resource_group.rg_web_app.name
   service_plan_id       = azurerm_service_plan.appserviceplan.id
   https_only            = true
-  site_config { 
-#     minimum_tls_version = "1.2"
-#     always_on = true
-#     virtual_application {
-#       physical_path = "site\\wwwroot"
-#       preload = true
-#       virtual_path = "/"
-#     }
-  }
+  site_config { }
 }
 
 #  Deploy code from a public GitHub repo
