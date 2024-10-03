05df8b20c307fde3d4b33c40de5a1d7846dab25c
Petri Pöllänen <petri.pollanen@atea.fi>
2024-10-03 13:29:58 +0300

    Added source control

--- web_app.tf
+++ web_app.tf
@@ -22,10 +22,10 @@ resource "azurerm_linux_web_app" "webapp" {
 }
 
 # Deploy code from a public GitHub repo
-# resource "azurerm_app_service_source_control" "sourcecontrol" {
-#   app_id             = azurerm_linux_web_app.webapp.id
-#   repo_url           = "https://github.com/PetriPollanenAtea/focus_demo_webapp_application/"
-#   branch             = "main"
-#   use_manual_integration = false
-#   use_mercurial      = false
-# }
+resource "azurerm_app_service_source_control" "sourcecontrol" {
+  app_id             = azurerm_linux_web_app.webapp.id
+  repo_url           = "https://github.com/PetriPollanenAtea/focus_demo_webapp_application/"
+  branch             = "main"
+  use_manual_integration = false
+  use_mercurial      = false
+}
