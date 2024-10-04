0c16f3290643d6f254412290290a37826448507c
Petri Pöllänen <petri.pollanen@atea.fi>
2024-10-04 12:11:52 +0300

    Fixed webapp name

--- webapp.tf
+++ webapp.tf
@@ -10,7 +10,7 @@ resource "azurerm_windows_web_app" "res-4" {
   ftp_publish_basic_authentication_enabled       = false
   https_only                                     = true
   location                                       = "westeurope"
-  name                                           = "ppo-test-webapp-45"
+  name                                           = "focus-demo-webapp"
   resource_group_name                            = azurerm_resource_group.rg_web_app.name
   service_plan_id                                = azurerm_service_plan.res-3.id
   webdeploy_publish_basic_authentication_enabled = false
