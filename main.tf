d93782adf63f068b86a4196a9a8bfd312a92c6ed
Petri Pöllänen <petri.pollanen@atea.fi>
2024-10-04 11:16:56 +0300

    Cleanup

--- main.tf
+++
@@ -1,6 +0,0 @@
-# Create resource groups for environment
-resource "azurerm_resource_group" "rg_web_app" {
-  provider = azurerm
-  location = var.location
-  name = "rg-focus-demo-webapp"
-}
--- terraform.tfvars
+++
@@ -1,2 +0,0 @@
-subscription_id = "323635f4-82ae-4b1a-a0fe-6a7d6f021834"
-location = "westeurope"
